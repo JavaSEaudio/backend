9316015c10cad5cce01236dc144291c7ca15beff
mrLysyi <cheburano@gmail.com>
2014-07-24 15:37:33 +0300

    рабочее логирование
    C:/var/... логи, примеры в мейн методе

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,38 @@
 import DAO.AudioDAO;
 import Entity.AudioEntity;
 
+import org.apache.log4j.Priority;
 import util.Factory;
 import org.apache.log4j.Logger;
 
 public class Main {
 
     //private static Logger logger = Logger.getLogger(Main.class);
-    private static Logger stacktracesLog = Logger.getLogger( "stacktraces." + Main.class.getName() );
-   // private static Logger httpLog = Logger.getLogger( "http." + Main.class.getName() );
+
+//    private static Logger log = Logger.getLogger( Main.class );
+//    private static Logger connectionsLog = Logger.getLogger( "connections." + Main.class.getName() );
+//    private static Logger stacktracesLog = Logger.getLogger( "stacktraces." + Main.class.getName() );
+//    private static Logger httpLog = Logger.getLogger("http." + Main.class.getName());
+//   private static Logger httpLog = Logger.getLogger( "http." + Main.class.getName() );
     public static void main(final String[] args) throws Exception {
+
+//        stacktracesLog.info("TESSSSSSSSSSTTTTTTT1");
+//        stacktracesLog.warn("TESSSSSSSSSSTTTTTTT2");
+//
+//        log.debug("TESSSSSSSSSSTTTTTTT3");
+//        connectionsLog.error("TESSSSSSSSSSTTTTTTT4");
+//        httpLog.info("TESSSSSSSSSSTTTTTTT5");
+        Logger.getLogger("com.vaannila.admin").error("To admin log1");
+        Logger.getLogger("com.vaannila.report").error("To report log2");
+
+
         /**
          * При работе с ДАО обращатся только через фабрику!!!
          * Не забываем менять create на update при повторном запуске,
          * а также дисконектить после выполнения
          */
 
-        AudioDAO aDAO = Factory.getInstance().getAudioDAO();
+//        AudioDAO aDAO = Factory.getInstance().getAudioDAO();
         //UserDAO uDao = Factory.getInstance().getUserDAO();
         //SessionDAO sDao = Factory.getInstance().getSessionDAO();
 
@@ -39,20 +55,20 @@ public class Main {
 //        uDao.add(new UserEntity("user6", "pass6", "us6@m.com", "", ""));
 //        uDao.add(new UserEntity("user7", "pass7", "us7@m.com", "", ""));
 //        uDao.add(new UserEntity("Привет", "пароль", "при8@m.com", "", ""));
-        aDAO.add(new AudioEntity("Name30", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name31", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name32", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name33", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name34", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name35", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name36", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name37", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name38", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name39", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name40", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name41", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name42", "Artis", "Album","cheta"));
-        aDAO.add(new AudioEntity("Name43", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name30", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name31", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name32", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name33", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name34", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name35", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name36", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name37", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name38", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name39", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name40", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name41", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name42", "Artis", "Album","cheta"));
+//        aDAO.add(new AudioEntity("Name43", "Artis", "Album","cheta"));
 
 //        sDao.add(new SessionEntity(2, "UniQueKey10002"));
 //
@@ -132,7 +148,7 @@ public class Main {
 
 //
 
-        stacktracesLog.info("TESSSSSSSSSSTTTTTTT");
+
 
         System.out.println("END");
         return;
--- src/rest/authoriz/Exit.java
+++ src/rest/authoriz/Exit.java
@@ -1,6 +1,7 @@
 package rest.authoriz;
 
 import DAO.SessionDAO;
+import org.apache.log4j.Logger;
 import util.Factory;
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
@@ -22,7 +23,10 @@ public class Exit {
         Cookie cook = new Cookie("name", "");
         NewCookie cookie = new NewCookie(cook, "", 0, false);
 
+        Logger.getLogger("com.vaannila.admin").error("To admin log1__from web");
         System.out.println("Deleted cookies");
+
+        Logger.getLogger("com.vaannila.report").error("To report log2__from web");
         return Response.ok().cookie(cookie).build();
     }
 }
